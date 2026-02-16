a10a0bc5c65820be89c6290e2a35538d4afc888f
Demyanovaf <demyanovaf@vsk.sibur.ru>
2026-02-16 22:05:00 +0300

    Add addiction validation on create (status == NEW && dtCreate == now())

--- study-project/study-project-biz/src/commonMain/kotlin/validation/ValidateDtCreateOnCreate.kt
+++ study-project/study-project-biz/src/commonMain/kotlin/validation/ValidateDtCreateOnCreate.kt
@@ -4,12 +4,13 @@ import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import ru.demyanovaf.kotlin.taskManager.common.MgrContext
 import ru.demyanovaf.kotlin.taskManager.common.NONE
+import ru.demyanovaf.kotlin.taskManager.common.models.MgrWorkMode
 import ru.demyanovaf.kotlin.taskManager.cor.ICorChainDsl
 import ru.demyanovaf.kotlin.taskManager.cor.worker
 
 fun ICorChainDsl<MgrContext>.validateDtCreateOnCreate(title: String) = worker {
     this.title = title
-    on { taskValidating.dtCreate == Instant.NONE }
+    on { taskValidating.dtCreate == Instant.NONE && workMode == MgrWorkMode.PROD}
     handle {
         taskValidating.dtCreate = Clock.System.now()
     }
--- study-project/study-project-biz/src/commonMain/kotlin/validation/ValidateStatusOnCreate.kt
+++ study-project/study-project-biz/src/commonMain/kotlin/validation/ValidateStatusOnCreate.kt
@@ -2,12 +2,13 @@ package ru.demyanovaf.kotlin.taskManager.biz.validation
 
 import ru.demyanovaf.kotlin.taskManager.common.MgrContext
 import ru.demyanovaf.kotlin.taskManager.common.models.MgrStatus
+import ru.demyanovaf.kotlin.taskManager.common.models.MgrWorkMode
 import ru.demyanovaf.kotlin.taskManager.cor.ICorChainDsl
 import ru.demyanovaf.kotlin.taskManager.cor.worker
 
 fun ICorChainDsl<MgrContext>.validateStatusOnCreate(title: String) = worker {
     this.title = title
-    on { taskValidating.status != MgrStatus.NEW }
+    on { taskValidating.status != MgrStatus.NEW && workMode == MgrWorkMode.PROD}
     handle {
         taskValidating.status = MgrStatus.NEW
     }
